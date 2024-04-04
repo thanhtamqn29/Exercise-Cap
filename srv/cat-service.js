ca0dfe3267f4e061796f189bd0c8ca44d9ae4aec
thanhtamqn29 <144640484+thanhtamqn29@users.noreply.github.com>
2024-04-04 11:41:12 +0700

    feat: deploy

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -16,6 +16,7 @@ module.exports = (srv) => {
   })
 
   // Add some discount for overstocked books
+  
   srv.after ('READ', 'Books', each => {
     if (each.stock > 111)  each.title += ' -- 11% discount!'
   })
